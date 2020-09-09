d0c5b92f609e1debaf01ef8f98cfdb4e7921a4cd
Christopher Henderson <chenderson5280@gmail.com>
2020-09-09 05:12:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-
+winner = ""
+passengers.each do |room, name|
+  if room == :suite_a && name.start_with("A")
+    winner = name
+  end
+end
 end
